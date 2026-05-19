6f716324a636b032a62b9545e59fa05cfa7a2e2c
raj <113851737+RAJTripathi3030@users.noreply.github.com>
2026-05-19 16:11:35 -0700

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> nums_map;
+        for(auto ele : nums) {
+            nums_map[ele]++;
+        }
+
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > nums_pq;
+        for(auto& kv : nums_map) {
+            nums_pq.push({kv.second, kv.first});
+
+            // If the size of pq gets > k elements then start popping cuz I only need k top freq elements
+            if(nums_pq.size() > k) nums_pq.pop();
+        }
+
+        vector<int> ans;
+        while(!nums_pq.empty()) {
+            ans.push_back( nums_pq.top().second );
+            nums_pq.pop();
+        }
+
+        return ans;
+    }
+};
